37ab60702dada60429129a8f4e341f54850bebd6
Milos Vasic <i@mvasic.ru>
2022-10-21 17:31:54 +0300

    Installation recipes.

--- Recipes/prepare.sh
+++ Recipes/prepare.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-echo "To be implemented!"
-exit 1
+echo "Preparing the 'JSON' library for the installation" && \
+  git submodule init && git submodule update && \
+  echo "The 'JSON' library is prepared for the installation"
